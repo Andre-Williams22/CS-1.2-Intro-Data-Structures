a88c179c7f7582b6ff4d40beec607b945cf11665
Andre Williams <awjrs22@gmail.com>
2019-11-02 12:30:46 -0700

    Chaning url

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def search():
     return jsonify({"success":True,"tweets":t})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 5000))
+    app.run(debug=True, host='127.0.0.1', port=os.getenv('PORT', 5000))
