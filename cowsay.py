f74b1bd22f35bd13423184f98408a29a544f839d
Andre Williams <awjrs22@gmail.com>
2019-11-02 13:45:33 -0700

    deleted unnecessary files out of root

--- cowsay.py
+++
@@ -1,7 +0,0 @@
-from cowpy import cow
-
-
-
-cheese = cow.Moose(thoughts=True, tongue=True, eyes='dead')
-msg = cheese.milk("I'm too stoned to eat real grass dude!")
-print(msg)
--- quote_user.py
+++
@@ -1,3 +0,0 @@
-import python_quote
-
-python_quote.random_python_quote()
